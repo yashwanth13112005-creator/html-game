11d903647911ae4aa4e37c77db5141abd757d857
Yashwanth L <yashwanth13112005@gmail.com>
2026-03-27 10:36:42 +0530

    Add source.py

---
+++ source.py
@@ -0,0 +1,9 @@
+# source.py
+# A very simple Python script
+
+def main():
+    print("Hello, world!")
+    print("This is my first Python file pushed to GitHub.")
+
+if __name__ == "__main__":
+    main()
